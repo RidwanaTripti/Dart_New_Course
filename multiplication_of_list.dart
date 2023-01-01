3fa4a7a5e3e61810d12874bf3cf2d9ab18c9b970
RidwanaTripti <ridwanaakter17@gmail.com>
2023-01-02 03:40:43 +0600

    hw

--- multiplication_of_list.dart
+++ multiplication_of_list.dart
@@ -1,9 +1,5 @@
 // void main(List<String> args) {
 //   List a = [1, 10, -5, 3, 10];
-//   // print(a[0] * a[1]);
-//   // print(a[1] * a[2]);
-//   // print(a[2] * a[3]);
-//   // print(a[3] * a[4]);
 //   for (int i = 0; i <= 5; i++) {
 //     // print("a${[i]}*a${[i + 1]} = ${a[i] * a[i + 1]}");
 //     print("${a[i]} * ${a[i + 1]} = ${a[i] * a[i + 1]}");
